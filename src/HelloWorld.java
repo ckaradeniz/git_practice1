cbb285ee5e27af4eb43b343317f783c13d53309b
ckaradeniz <cengizkaradeniz001@gmail.com>
2022-04-02 14:14:40 +0200

    work 3 is done and about to be pushed to GitHub after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,10 @@ public class HelloWorld {
 
     //doing awesome work 2!
 
+    //Doing aweasome work 3!
+    //Awesome work 3 is done and ready to be committed
+    //and ready to be pushed to remote repository
+
 
 
 }
